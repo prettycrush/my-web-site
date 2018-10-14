daee4456e7c6b0f53804e006891b6bbc7ad509f0
Olena Mishchak <olena.krasheninnikova@gmail.com>
2018-10-14 13:43:27 +0000

    my web site

--- js/price.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.run-query').addEventListener('click', function() {
-   fetch(document.querySelector('.url').value)
-      .then( response => response.json() )
-      .then( result => {
-          document.querySelector('.public-api-result').innerText = JSON.stringify(result);
-  }); 
-});
